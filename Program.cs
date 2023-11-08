71bb07332d6a43ef9d2d496eadedd87c6057a48e
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-08 09:27:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbaCetvrta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite recenicu");
+            string recenica = Console.ReadLine();
+            string prva = recenica.Substring(0, recenica.IndexOf(" "));
+            string zadnja = recenica.Substring(recenica.LastIndexOf(" ", recenica.Length - 1));
+            Console.WriteLine(prva + zadnja);
+            Console.ReadKey();
+        }
+    }
+}
